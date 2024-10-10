67f5b2fa4b1b72e5cd42cd4a6fa591314940acf5
Dani-Mol <78124826+Dani-Mol@users.noreply.github.com>
2024-10-10 00:23:55 -0600

    Comentarios para entender el código

--- saltos.py
+++ saltos.py
@@ -1,12 +1,15 @@
 def saltos (arr):
     indice = 0
-    while indice < len(arr):
+    while indice < len(arr): 
+        # Cuando el indice se salga del rango o si no es posible avanzar mas termina el ciclo.
         salto = arr[indice]
-        if salto == 0:
+        if salto == 0: 
+        # Falso para no quedar atrapado en el mismo lugar (por no poder avanzar)
             return False
         indice += salto
         
     return indice >= len(arr) 
+    # Retorna True si el indice es mayor o igual y False si la condicion while no se cumple
 
 
 
